30f9a9be0a1ff4f4d6d1c7f8b74dea0df6ae25b9
unknown <johnw@L411.ehi.ehealth.com>
2018-02-07 16:37:19 +0800

    add comment

--- main.py
+++ main.py
@@ -9,32 +9,17 @@ from urllib.parse import unquote,urlparse
 import re
 import os
 from bs4 import BeautifulSoup
-'''
-def get_referer_address(start,total):
-    referer = []
-    for page in range(start,total):
-        r = requests.get('https://www.dongmanmanhua.cn/comedy/xin-xinlingdeshengyin/list?title_no=381&page='+str(page),verify=False)
-        soup = BeautifulSoup(r.text,'html.parser')
-        ul = soup.find(id='_listUl')
-        all_li = ul.find_all('li')
-        for li in all_li:
-            #print(li.find('a').get('href'))
-            with open('test.txt','a',encoding='utf-8')as f:
-                f.write('https:'+li.find('a').get('href')+'\n')
+requests.packages.urllib3.disable_warnings()
 
-            referer_url = 'https:'+li.find('a').get('href')
-            referer.append(referer_url)
-    return referer
-'''
 url = 'https://www.dongmanmanhua.cn/comedy/xin-xinlingdeshengyin/list?title_no=381&page='
 
 # url is referer url
 '''
 @url : 这里的url是refer的url。也就是每一页具体的漫画
+返回创建的文件夹的名字
 '''
 def create_dir(url):
     o = urlparse(url)
-    #print(o.path)
     s = re.match(r'\/comedy\/xin-xinlingdeshengyin\/(.*?)\/viewer',o.path)
     os.mkdir(unquote(s.group(1)))
     return unquote(s.group(1))
@@ -43,6 +28,7 @@ def create_dir(url):
 '''
 @url 这里的url参数是refer的url，也是每一页具体的漫画
 @page 这里的page是分页的页码数
+返回每一页具体漫画的地址列表
 '''
 def get_referer_address(url,page):
     referers = []
@@ -56,9 +42,9 @@ def get_referer_address(url,page):
 
     return referers
 
-# 得到单个refer的地址，但是图片地址是多个
 '''
-@url 这里的url是refer的url，
+@url 这里的url是refer的url
+@返回每个图片的实际地址
 '''
 def get_img(url):
     data_url = []
@@ -70,11 +56,9 @@ def get_img(url):
         data_url.append(img.get('data-url'))
     return data_url
 
-# 开始保存图片
 '''
-
+@referers 指的是一个分页里面的每个漫画的url
 '''
-
 def save_img(referers):
     for referer in referers:
         headers = {
@@ -94,8 +78,4 @@ if __name__ == "__main__":
         referers = get_referer_address(url,num)
         save_img(referers)
 
-
-
-
-#get_referer_address(1,23)
 
